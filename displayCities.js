1df3e99ca8524a184ec86ea79f5c7c334e21b8a7
Furkan Yaglidere <115979551+11Furkan@users.noreply.github.com>
2023-09-22 16:37:34 +0200

    add filrter and refector

---
+++ displayCities.js
@@ -0,0 +1,50 @@
+import { Feature } from 'ol/index.js';
+import { Point } from 'ol/geom.js';
+
+import { City } from './City.js';
+import { citySource } from './map.js';
+
+const citiesURL = 'https://ydz8vgh5cd.execute-api.eu-central-1.amazonaws.com/cities'
+let feature 
+
+
+function toDaily(hours){
+  return parseFloat((hours/30.437).toFixed(1));
+}
+
+function toMonthly(days){
+  return parseFloat((days/12).toFixed(1));
+}
+
+export function displayCities(MinSunHours, MaxSunHours, MinRainDays, MaxRainDays) {
+  // clear layer 
+  citySource.clear()
+  let featureList = []
+
+  console.log(MinSunHours, MaxSunHours, MinRainDays, MaxRainDays)
+
+  fetch(citiesURL)
+    // to JSON
+    .then(res => res.json())
+    // but still marshalled ; uncomment to see in console
+    // .then(json => console.log(json.Items[0].geography.M.rainy_days.N))
+    .then(json => {
+      const cities = json.Items.map(item => new City(item));
+      cities.forEach((c) => {
+        // console.log(c.cname)
+        feature = new Feature(new Point([c.longitude, c.latitude]));
+        // console.log(feature.getProperties())
+        c.rainy_days = toMonthly(c.rainy_days)
+        c.sun_hours = toDaily(c.sun_hours)
+        feature.setProperties(c);
+
+        // if (MinRainDays <= c.rainy_days && toMonthly(c.rainy_days) <= MaxRainDays && MinSunHours <= c.sun_hours && c.sun_hours <= MaxSunHours) {
+        if (MinRainDays <= c.rainy_days && c.rainy_days <= MaxRainDays && MinSunHours <= c.sun_hours && c.sun_hours <= MaxSunHours) {
+
+          featureList.push(feature);
+        }
+      });
+      console.log(featureList.length)
+      citySource.addFeatures(featureList);
+    });
+}
---
+++ map.js
@@ -0,0 +1,60 @@
+import { Map, View } from 'ol/index.js';
+import { OSM, Vector as VectorSource } from 'ol/source.js';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer.js';
+import LayerGroup from 'ol/layer/Group';
+import {useGeographic} from 'ol/proj.js';
+
+useGeographic();
+// const center = ["6.13","49.61"]
+const center = ["14.44","50.08"]
+
+// Map
+export const map = new Map({
+  target: 'map',
+  view: new View({
+    center: center,
+    zoom: 4.6,
+  })
+});
+
+// Source
+export const citySource = new VectorSource();
+
+// Layers
+const openStreetMapStandard = new TileLayer({
+  source: new OSM(),
+  visible: true,
+  title: "OSMStandard"
+});
+
+// const openStreetCycle = new TileLayer({
+//   source: new OSM({
+//     url: 'http://{a-c}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png'
+//   }),
+//   visible: true,
+//   title: 'OSMCycle'
+// });
+const cityLayer = new VectorLayer({
+  style: {
+    'circle-radius': 3,
+    'circle-fill-color': 'blue'
+  },
+  source: citySource,
+  visible:true
+});
+
+// LayerGroup
+const baseLayerGroup = new LayerGroup({
+  // layers: [openStreetMapStandard, openStreetCycle, cityLayer]
+  layers: [openStreetMapStandard, cityLayer]
+});
+map.addLayer(baseLayerGroup);
+
+
+// change Pointer over Feature
+map.on('pointermove', function (event) {
+  const type = map.hasFeatureAtPixel(event.pixel) ? 'pointer' : 'inherit';
+  map.getViewport().style.cursor = type;
+});
+
+
